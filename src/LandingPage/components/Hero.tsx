519d83e13cebc70e0b2911799169cecfd0267e9e
bukolastella <abolarinbukolastella@gmail.com>
2023-08-09 23:16:02 +0100

    nav safe

--- src/LandingPage/components/Hero.tsx
+++ src/LandingPage/components/Hero.tsx
@@ -24,11 +24,6 @@ const Hero = () => {
       let animation = gsap.timeline({
         defaults: {
           ease: "circ.in",
-          // ease: "none",
-          // ease: CustomEase.create(
-          //   "custom",
-          //   "M0,0 C0,0 0,0.034 0.066,0.126 0.071,0.151 0.144,0.212 0.156,0.234 0.163,0.248 0.214,0.289 0.226,0.3 0.416,0.478 0.486,0.496 0.66,0.684 0.671,0.696 0.725,0.757 0.736,0.768 0.749,0.781 0.791,0.826 0.8,0.846 0.81,0.869 0.851,0.915 0.856,0.942 0.872,1.036 1,0.99 1,0.99 "
-          // ),
           duration: duration,
           stagger: {
             each: stagger,
@@ -38,17 +33,17 @@ const Hero = () => {
         },
       });
 
-      animation
-        .from(targets, {
-          x: "100vw",
-        })
-        .to(
-          targets,
-          {
-            x: "-100vw",
-          },
-          stagger
-        );
+      // animation
+      //   .from(targets, {
+      //     x: "100vw",
+      //   })
+      //   .to(
+      //     targets,
+      //     {
+      //       x: "-100vw",
+      //     },
+      //     stagger
+      //   );
     }, container);
 
     return () => ctx.revert();
--- src/LandingPage/components/Nav.tsx
+++ src/LandingPage/components/Nav.tsx
@@ -1,15 +1,131 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
 import { styled } from "styled-components";
 
 const Nav = () => {
+  const container = useRef<HTMLImageElement>(null);
+  const animation = useRef<any>(null);
+  const stretch = useRef<any>(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context((self) => {
+      const boxes = self.selector && self.selector(".box");
+      const wrap = self.selector && self.selector(".wrap");
+
+      gsap.timeline().fromTo(
+        container.current,
+        {
+          opacity: 0,
+          ease: "none",
+        },
+        {
+          opacity: 1,
+          bottom: "300px",
+          ease: "none",
+        }
+      );
+
+      animation.current = gsap.timeline({ repeat: -1 });
+
+      boxes.forEach((element: HTMLElement, index: number) => {
+        const boxTl = gsap
+          .timeline()
+          .to(element, {
+            "--opacity": 1,
+            ease: "none",
+          })
+          .to(element, {
+            "--width": "100%",
+            ease: "none",
+            duration: 5,
+          });
+
+        stretch.current = gsap
+          .timeline()
+          .set(wrap, {
+            x: boxes[index].offsetLeft,
+            width: boxes[index].clientWidth,
+            opacity: 1,
+          })
+          .to(wrap, {
+            x: boxes[index === boxes.length - 1 ? 0 : index + 1].offsetLeft,
+            width:
+              boxes[index === boxes.length - 1 ? 0 : index + 1].clientWidth,
+          })
+          .to(
+            element,
+            {
+              color: "white",
+              ease: "none",
+              duration: 0.1,
+            },
+            "<"
+          )
+          .to(
+            element,
+            {
+              "--opacity": 0,
+              color: "#cdcccc",
+              ease: "none",
+              duration: 0.1,
+            },
+            "<"
+          )
+          .to(
+            wrap,
+            {
+              opacity: 0,
+            },
+            "=-0.2"
+          );
+
+        animation.current?.addLabel(`${index}`).add(boxTl).add(stretch.current);
+
+        const toggle = (el: any) => {
+          animation.current.progress(1);
+          animation.current.kill();
+          const clickedElement = el.target;
+          const temp = gsap
+            .timeline()
+            .to(wrap, {
+              opacity: 1,
+              x: clickedElement.offsetLeft,
+              width: clickedElement.clientWidth,
+            })
+            .to(
+              wrap,
+              {
+                opacity: 0,
+                onComplete: () => animation.current?.play(`${index}`),
+              },
+              "=-0.2"
+            );
+          temp.play();
+        };
+
+        element.addEventListener("click", toggle);
+      });
+
+      // return () =>
+      //   boxes.forEach((element: any) =>
+      //     element.removeEventListener("click", toggle)
+      //   );
+    }, container);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <Container>
+    <Container ref={container}>
       <Wrapper>
-        <div>Snacking tomatoes</div>
-        <div>Citrus</div>
-        <div>Grapes</div>
-        <div>Avocados</div>
-        <div>Flowers</div>
+        <div className="wrap"></div>
+        <div className="box">Snacking tomatoes</div>
+        <div className="box" onClick={() => {}}>
+          Citrus
+        </div>
+        <div className="box">Grapes</div>
+        <div className="box">Avocados</div>
+        <div className="box">Flowers</div>
       </Wrapper>
       <Button>Discover</Button>
     </Container>
@@ -24,7 +140,7 @@ const Container = styled.div`
   align-items: center;
   gap: 40px;
   z-index: 10;
-  bottom: 300px;
+  bottom: 100px;
   left: 200px;
 `;
 
@@ -32,26 +148,49 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
+  position: relative;
 
-  & > div {
+  .wrap {
+    background-color: white;
+    height: 2px;
+    position: absolute;
+    bottom: -10px;
+    opacity: 1; //
+    width: 200px; //
+    left: 0;
+  }
+
+  .box {
+    cursor: pointer;
     position: relative;
     font-size: 1.5rem;
+    color: #cdcccc;
+    --width: 0;
+    --opacity: 0;
+    --left: 0;
+    --right: unset;
+
     &::before,
     &::after {
       content: "";
       position: absolute;
       left: 0;
+      left: var(--left);
       bottom: -10px;
-      width: 100%;
       height: 2px;
+      opacity: var(--opacity);
     }
 
     &::before {
-      background-color: red;
+      width: 100%;
+      background-color: #908f8f;
     }
 
     &::after {
+      width: var(--width);
       background-color: white;
+      left: var(--left);
+      right: var(--right);
     }
   }
 `;
